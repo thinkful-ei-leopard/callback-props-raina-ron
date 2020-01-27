3617b97d79cf83225f73d5ad360b6b6f7e202c1a
wavinginspace <paulbaisley@gmail.com>
2020-01-27 14:19:31 -0500

    project setup

---
+++ src/List.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function List() {
+  
+}
+
+
+export default List;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import List from './List';
+import Card from './Card';
+import STORE from './store';
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
