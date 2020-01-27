ace064394eb3ec0c9b82850b9f740a000a4cc9bb
wavinginspace <paulbaisley@gmail.com>
2020-01-27 15:03:39 -0500

    app edits

--- src/App.js
+++ src/App.js
@@ -5,12 +5,21 @@ import STORE from './store';
 
 
 function App(STORE) {
+  const cards = [];
+  
   return (
     <main className="App">
     <header className="App-header">
       <h1>Trelloyes!</h1>
     </header>
     <div className="App-list">
+      STORE.lists.forEach(list => 
+      <List header={list.header}
+      card={list.cardIds.find(cardId => 
+        cardId ===
+        // LEFT OFF HERE
+        )}
+      />)
     </div>
     </main>
   );
