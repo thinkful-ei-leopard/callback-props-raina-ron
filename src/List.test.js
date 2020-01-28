402da8a488306ac4d128a87d8f27138a4c58ad56
Ron Martin <ronmartin89@gmail.com>
2020-01-28 08:41:30 -0800

    Fixed List test

--- src/List.test.js
+++ src/List.test.js
@@ -7,7 +7,7 @@ describe('<List />', () => {
     it('renders without crashing', () => {
         const div = document.createElement('div');
 
-        ReactDOM.render(<List {{id: "a", title: "First card", content: "lorem ipsum"}}  />, div);
+        ReactDOM.render(<List cards={[{id: "a", title: "First card", content: "lorem ipsum"}]}  />, div);
 
         ReactDOM.unmountComponentAtNode(div);
     });
